1e415d1fabecdd31f6d169495a14b2d85696c5bb
tbysss <maytby@gmail.com>
2024-07-11 00:02:09 +0200

    removed index page, was more annoying than functional update rust to latest can now also upload 7z files

--- webserver-rocket/src/main.rs
+++ webserver-rocket/src/main.rs
@@ -1,28 +1,45 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::Config;
-use rocket::form::{Context, Contextual, Form};
+use rocket::form::{Context, Contextual, Error, Errors, Form};
 use rocket::fs::{relative, FileServer, TempFile};
 use rocket::http::{ContentType, Status};
-
+use rocket::Config;
 use rocket_dyn_templates::Template;
 
 #[derive(Debug, FromForm)]
 #[allow(dead_code)]
 struct Submission<'v> {
-    #[field(validate = len(1..))]
+    #[field(validate = len(1..).or_else(msg!("project required")))]
     project: &'v str,
-    #[field(validate = len(1..))]
+    #[field(validate = len(1..).or_else(msg!("gate required")))]
     gate: &'v str,
-    #[field(validate = ext(ContentType::ZIP))]
+    #[field(validate = accept_content_types())]
     file: TempFile<'v>,
 }
 
 #[derive(Debug, FromForm)]
 #[allow(dead_code)]
 struct Submit<'v> {
-    submission: Submission<'v>
+    submission: Submission<'v>,
+}
+
+fn accept_content_types<'v>(file: &TempFile<'_>) -> Result<(), Errors<'v>> {
+    let seven_z = ContentType::new("application", "x-7z-compressed");
+    let zip = ContentType::ZIP;
+    if let Some(file_ct) = file.content_type() {
+        if file_ct == &seven_z || file_ct == &zip {
+            return Ok(());
+        }
+    }
+
+    let msg = match (file.content_type().and_then(|c| c.extension()), seven_z.extension(), zip.extension()) {
+        (Some(a), Some(b), Some(c)) => format!("invalid file type: .{}, must be .{} or .{}", a, b, c),
+        (Some(a), None, None) => format!("invalid file type: .{}, must be {} or {}", a, seven_z, zip),
+        _ => format!("file type must be .{} or .{}", "7z", "zip"),
+    };
+
+    Err(Error::validation(msg))?
 }
 
 #[get("/")]
@@ -35,13 +52,15 @@ fn submit<'r>(form: Form<Contextual<'r, Submit<'r>>>, config: &Config) -> (Statu
     let template = match form.value {
         Some(ref submission) => {
             let file_name = submission.submission.file.name().unwrap();
+            // fallback to 7z, since ContentType does not know 7z
             let extension = submission
                 .submission
                 .file
                 .content_type()
                 .and_then(|f| f.extension())
-                .unwrap()
-                .as_str();
+                .map(|s| s.as_str())
+                .or_else(|| Some("7z"))
+                .unwrap();
             let file_name_with_extension = vec![file_name, extension].join(".");
             let file_path: &std::path::Path = submission.submission.file.path().unwrap();
 
@@ -49,23 +68,16 @@ fn submit<'r>(form: Form<Contextual<'r, Submit<'r>>>, config: &Config) -> (Statu
             p.push(submission.submission.project);
             p.push(submission.submission.gate);
             // already exists -> simply show success
-            if std::path::Path::new(&p.join(&file_name_with_extension)).is_file() {
-                Template::render("success", &form.context)
-            } else {
+            if !std::path::Path::new(&p.join(&file_name_with_extension)).is_file() {
                 println!("submission: {:#?}", submission);
                 std::fs::create_dir_all(p.clone()).expect("upload dir created");
                 p.push(file_name_with_extension);
-                println!(
-                    "persiting uploaded data file {:?} to {:?}",
-                    file_name, p
-                );
+                println!("persisting uploaded data file {:?} to {:?}", file_name, p);
                 std::fs::rename(file_path, p).expect("failed to persist upload data");
-                Template::render("success", &form.context)
             }
-        }
-        None => {
             Template::render("index", &form.context)
         }
+        None => Template::render("index", &form.context),
     };
 
     (form.context.status(), template)
